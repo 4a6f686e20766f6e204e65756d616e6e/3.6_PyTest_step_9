276de3976d3ab39e4633447f72bd34e13d03dc9f
Iashchenko <diashchenko@neoflex.ru>
2022-05-10 18:31:46 +0400

    Changed the name of the main function

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import pytest
 from selenium import webdriver
 import time
 
-def test_see_shopping cart(browser):
+def test_see_shopping_cart(browser):
 	link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 	browser.get(link)
 	button = browser.find_element_by_css_selector("button.btn-add-to-basket")
