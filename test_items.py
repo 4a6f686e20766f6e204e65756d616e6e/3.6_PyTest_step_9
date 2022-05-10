7d8e90c7f10c164a10166b63758a3e9159ca35ad
Iashchenko <diashchenko@neoflex.ru>
2022-05-10 18:30:21 +0400

    Added conftest.py and test_items.py

--- test_items.py
+++ test_items.py
@@ -2,9 +2,9 @@ import pytest
 from selenium import webdriver
 import time
 
-def test_guest_should_see_login_link(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-    browser.get(link)
-    button = browser.find_element_by_css_selector("button.btn-add-to-basket")
-    # time.sleep(30) - раскоментировать для проверки 
-    assert button.is_displayed(), "Button add to basket not displayed."
+def test_see_shopping cart(browser):
+	link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+	browser.get(link)
+	button = browser.find_element_by_css_selector("button.btn-add-to-basket")
+	# time.sleep(30) - раскоментировать для проверки 
+	assert button.is_displayed(), "Button add to basket not displayed."
